e7f77c39e50cc8b6f82738d756ff0bcfbce60288
rebecca-pete <rebecca@rebeccapeterson.us>
2019-06-04 21:36:06 -0700

    refactoring, incomplete

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const fs = require('fs');
+const net = require('net');
+
+// const testFile = require('./test.txt');
 
 const alterFile = (file) => {
   fs.readFile( file, (err, data) => {
@@ -13,5 +16,15 @@ const alterFile = (file) => {
   });
 };
 
+console.log(process.argv);
 let file = process.argv.slice(2).shift();
-alterFile(file);
+const client = net.createConnection({port:3001},() => {
+  console.log('Connected');
+  client.on('data', (data) => {
+    console.log('I got data! ', data);
+  });
+  client.write('hello world');
+  // alterFile(file);
+}); 
+
+// alterFile(file);
--- server.js
+++ server.js
@@ -9,19 +9,32 @@ server.listen(port, () => console.log(`Server up on ${port}`) );
 
 let socketPool = {};
 
+const allowedCommands = [
+  'READ',
+  'UPPERCASE',
+  'WRITE',
+];
+
+//defining what actions to take based on actions that are emitted
 server.on('connection', (socket) => {
+
   const id = `Socket-${Math.random()}`;
   socketPool[id] = socket;
+  console.log('socket pool keys: ', Object.keys(socketPool));
+  //emitting data events
   socket.on('data', (buffer) => dispatchEvent(buffer));
   socket.on('close', () => {
     delete socketPool[id];
   });
 });
 
+//emitting events
 let dispatchEvent = (buffer) => {
+  console.log('I dispatched an event');
   let text = buffer.toString().trim();
   for (let socket in socketPool) {
-    socketPool[socket].write(`${event} ${text}`);
+    //still need to define event, currently 'data' string
+    socketPool[socket].write(`${'data'} ${text}`);
   }
 };
 
